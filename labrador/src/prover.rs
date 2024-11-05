3294f56267e3ce7738222bba66a54ddff71f7531
Harry <2411mail@gmail.com>
2024-11-05 10:51:28 +0700

    refactor

---
+++ labrador/src/prover.rs
@@ -0,0 +1,17 @@
+// src/prover.rs
+
+pub fn prove() {
+    println!("Proving something...");
+    // Add your proving logic here
+}
+
+// add test cases
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_prove() {
+        prove();
+    }
+}
---
+++ labrador/src/verifier.rs
@@ -0,0 +1,6 @@
+// src/verifier.rs
+
+pub fn verify() {
+    println!("Verifying something...");
+    // Add your verifying logic here
+}
