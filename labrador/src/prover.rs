76587d9c22b52be52e30fb1463a2022b19ae3cd5
Harry <2411mail@gmail.com>
2024-11-05 11:06:24 +0700

    add comments

--- labrador/src/prover.rs
+++ labrador/src/prover.rs
@@ -3,15 +3,16 @@
 pub fn prove() {
     println!("Proving something...");
     // Add your proving logic here
-}
+    // 1. setup constraints
+    // 1.1 get s_i and do norm check
+    // 1.1.1 get s_i = s_1 - s_r; r is the number of witness s
+    // each s_i is a vector of ring elements
 
-// add test cases
-#[cfg(test)]
-mod tests {
-    use super::*;
+    // 1.1.2 get beta norm bound, refer paper page 26, theorem 6.3
 
-    #[test]
-    fn test_prove() {
-        prove();
-    }
+    // 1.1.3 do check: sum of s_i norm <= beta_square
+
+    // 1.2 calculate b^(k)
+
+    // 1.3 calculate b'^(l)
 }
