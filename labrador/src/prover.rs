ca120b816eb0fbdda2acc3cd7eafef42f06ed188
Harry <2411mail@gmail.com>
2024-11-05 12:29:16 +0700

    add comments

--- labrador/src/prover.rs
+++ labrador/src/prover.rs
@@ -13,6 +13,20 @@ pub fn prove() {
     // 1.1.3 do check: sum of s_i norm <= beta_square
 
     // 1.2 calculate b^(k)
+    // 1.2.1 calculate dot product ss = a_ij * <s_i, s_j> for all i, j
+    // a_ij is the quadratic coefficient, phi^(k) is the linear coefficient
+    // 1.2.2 calculate phi_s = <phi^(k), s_i> for all i
+    // 1.2.3 calculate b^(k) = sum(ss) + sum(phi_s)
 
     // 1.3 calculate b'^(l)
+    // 1.3.1 calculate dot product ss = a_ij' * <s_i, s_j> for all i, j
+    // a_ij' is the quadratic coefficient, phi^(l)' is the linear coefficient
+    // 1.3.2 calculate phi_s = <phi^(l)', s_i> for all i
+    // 1.3.3 calculate b'^(l) = sum(ss) + sum(phi_s)
+
+    // 2. calculate u1
+    // 2.1 split t to t_i for all i
+    // 2.1.1 get basis b1, refer to paper page 16, labrador c code line 142
+    // refer to note: line => xxx
+
 }
