68364ec5f5f662b6cc665bee3b0c57ce7576c318
Harry <2411mail@gmail.com>
2024-11-09 17:04:40 +0700

    text: update comments

--- labrador/src/prover.rs
+++ labrador/src/prover.rs
@@ -172,7 +172,7 @@ impl RqMatrix {
     }
 }
 
-// calculate A matrix times s_i
+// Ajtai commitment: calculate A matrix times s_i
 fn calculate_a_times_s_i(a: &RqMatrix, s_i: &Vec<RingPolynomial>) -> Vec<RingPolynomial> {
     a.values.iter().map(|row| {
         row.iter().zip(s_i.iter()).map(|(a, b)| a.multiply_by_ringpolynomial(b)).collect::<Vec<RingPolynomial>>()
@@ -181,12 +181,12 @@ fn calculate_a_times_s_i(a: &RqMatrix, s_i: &Vec<RingPolynomial>) -> Vec<RingPol
 
 // convert number to basis
 // 42 = 0 * 2^7 + 1 * 2^6 + 0 * 2^5 + 1 * 2^4 + 0 * 2^3 + 1 * 2^2 + 0 * 2^1 + 0 * 2^0
-// first step: 42 / 2 = 21, result_i = 0
-// second step: 21 / 2 = 10, result_i = 1
-// third step: 10 / 2 = 5, result_i = 0
-// forth step: 5 / 2 = 2, result_i = 1
-// fifth step: 2 / 2 = 1, result_i = 0
-// sixth step: 1 / 2 = 0, result_i = 1
+// first digit: 42 / 2 = 21, result_i = 0
+// second digit: 21 / 2 = 10, result_i = 1
+// third digit: 10 / 2 = 5, result_i = 0
+// forth digit: 5 / 2 = 2, result_i = 1
+// fifth digit: 2 / 2 = 1, result_i = 0
+// sixth digit: 1 / 2 = 0, result_i = 1
 
 fn num_to_basis(num: usize, basis: usize, digits: usize) -> Vec<usize> {
     let mut result = Vec::new();
@@ -319,7 +319,7 @@ mod tests {
         // t1: length of t[i][j][k], such as length of t[0][0][0] = 3
         // Then:
         // t_0_basis_form: [
-        //   [[8, 0, 0], [4, 6, 0], [4, 1, 1], [0, 0, 1], [7, 6, 0], [3, 2, 0], [6, 0, 0]],
+        //   [[8, 0, 0], [4, 6, 0], [6, 1, 0], [7, 1, 0], [3, 3, 0], [3, 3, 0], [1, 8, 0]],
         //   [[5, 4, 0], [4, 8, 0], [4, 3, 1], [9, 0, 1], [9, 8, 0], [7, 4, 0], [4, 1, 0]],
         //   [[6, 0, 0], [3, 4, 0], [0, 8, 0], [5, 2, 1], [5, 2, 1], [8, 9, 0], [8, 4, 0]],
         //   [[8, 2, 0], [0, 6, 0], [9, 8, 0], [1, 8, 0], [0, 0, 1], [3, 8, 0], [3, 6, 0]],
