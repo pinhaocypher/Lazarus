d80e869fcfb683e3cd5de23b8e88540064dce047
Harry <2411mail@gmail.com>
2024-12-23 20:37:36 +0700

    refactor: put outer commitment u1 calculation into a function

--- labrador/src/prover.rs
+++ labrador/src/prover.rs
@@ -526,6 +526,91 @@ fn check_aggr_relation(a_aggr: &Vec<Vec<PolynomialRing>>, b_aggr: PolynomialRing
     assert_eq!(sum_a_ij_g_ij + sum_h_ii, b_aggr);
 }
 
+// 2.3 calculate u1
+// 2.3.1 B & C is randomly chosen similar to A
+// 2.3.2 calculate u1 = sum(B_ik * t_i^(k)) + sum(C_ijk * g_ij^(k))
+// B_ik: Rq^{kappa1 x kappa}, t_i: Rq^{kappa}, t_i^(k): Rq^{kappa}
+// B_ik * t_i^(k): Rq^{kappa1}
+// First summation: ∑ B_ik * t_i^(k), 1 ≤ i ≤ r, 0 ≤ k ≤ t1−1
+// Initialize u1 with zeros with size kappa1, each element is a polynomial ring
+fn calculate_outer_comm_u1(
+    b_matrix: &Vec<Vec<RqMatrix>>,
+    c_matrix: &Vec<Vec<Vec<RqMatrix>>>,
+    g_matrix_aggregated: &Vec<Vec<Vec<PolynomialRing>>>,
+    all_t_i_basis_form_aggregated: &Vec<Vec<Vec<PolynomialRing>>>,
+    kappa1: Zq,
+    t1: Zq,
+    t2: Zq,
+    size_r: Zq,
+    size_n: Zq,
+) -> Vec<PolynomialRing> {
+
+    let mut u1 = vec![
+        PolynomialRing {
+            coefficients: vec![Zq::from(0); size_n.value()]
+        };
+        kappa1.value()
+    ];
+    // Calculate u1 using the pre-generated b_matrix
+    for i in 0..size_r.value() {
+        for k in 0..t1.value() {
+            let b_i_k = &b_matrix[i][k];
+            let t_i_k = &all_t_i_basis_form_aggregated[i][k];
+            // matrix<Rq> * vector<Rq> -> vector<Rq>
+            let b_ik_times_t_ik = b_i_k.values
+                .iter()
+                .map(|row| {
+                    row.iter()
+                        .zip(t_i_k.iter())
+                        .map(|(b, t)| b * t)
+                        .fold(
+                            PolynomialRing {
+                                coefficients: vec![Zq::from(0); size_n.value()],
+                            },
+                            |acc, val| acc + val,
+                        )
+                })
+                .collect::<Vec<PolynomialRing>>();
+            u1 = u1
+                .iter()
+                .zip(b_ik_times_t_ik.iter())
+                .map(|(a, b)| a + b)
+                .collect();
+        }
+    }
+
+    // Second summation: ∑ C_ijk * g_ij^(k)
+    // Calculate u1 using the pre-generated c_matrix
+    for i in 0..size_r.value() {
+        for j in i..size_r.value() {
+            for k in 0..t2.value() {
+                let c_i_j_k = &c_matrix[i][j][k];
+                let g_i_j = &g_matrix_aggregated[i][j];
+                let c_i_j_k_times_g_i_j = c_i_j_k.values
+                    .iter()
+                    .map(|row| {
+                        row.iter()
+                            .zip(g_i_j.iter())
+                            .map(|(c, g)| c * g)
+                            .fold(
+                                PolynomialRing {
+                                    coefficients: vec![Zq::from(0); size_n.value()],
+                                },
+                                |acc, val| acc + val,
+                            )
+                    })
+                    .collect::<Vec<PolynomialRing>>();
+                u1 = u1
+                    .iter()
+                    .zip(c_i_j_k_times_g_i_j.iter())
+                    .map(|(a, b)| a + b)
+                    .collect();
+            }
+        }
+    }
+
+    u1
+}
 
 // calculate u2 = sum D_ij * h_ij^(k) for all k = 1..(t1-1)
 fn calculate_outer_comm_u2(
@@ -745,77 +830,18 @@ pub fn prove(a_matrix: &RqMatrix, b_matrix: &Vec<Vec<RqMatrix>>, c_matrix: &Vec<
     let g_matrix_aggregated = poly_matrix_decompose_and_aggregate(&g, basis, t2);
 
     // 2.3 calculate u1
-    // 2.3.1 B & C is randomly chosen similar to A
-    // 2.3.2 calculate u1 = sum(B_ik * t_i^(k)) + sum(C_ijk * g_ij^(k))
-    // B_ik: Rq^{kappa1 x kappa}, t_i: Rq^{kappa}, t_i^(k): Rq^{kappa}
-    // B_ik * t_i^(k): Rq^{kappa1}
-    // First summation: ∑ B_ik * t_i^(k), 1 ≤ i ≤ r, 0 ≤ k ≤ t1−1
-    // Initialize u1 with zeros with size kappa1, each element is a polynomial ring
+    let u1 = calculate_outer_comm_u1(
+        &b_matrix,
+        &c_matrix,
+        &g_matrix_aggregated,
+        &all_t_i_basis_form_aggregated,
+        kappa1,
+        t1,
+        t2,
+        size_r,
+        size_n
+    );
     println!("Prover: Send proof u1");
-    let mut u1 = vec![
-        PolynomialRing {
-            coefficients: vec![Zq::from(0); size_n.value()]
-        };
-        kappa1.value()
-    ];
-    // Calculate u1 using the pre-generated b_matrix
-    for i in 0..size_r.value() {
-        for k in 0..t1.value() {
-            let b_i_k = &b_matrix[i][k];
-            let t_i_k = &all_t_i_basis_form_aggregated[i][k];
-            // matrix<Rq> * vector<Rq> -> vector<Rq>
-            let b_ik_times_t_ik = b_i_k.values
-                .iter()
-                .map(|row| {
-                    row.iter()
-                        .zip(t_i_k.iter())
-                        .map(|(b, t)| b * t)
-                        .fold(
-                            PolynomialRing {
-                                coefficients: vec![Zq::from(0); size_n.value()],
-                            },
-                            |acc, val| acc + val,
-                        )
-                })
-                .collect::<Vec<PolynomialRing>>();
-            u1 = u1
-                .iter()
-                .zip(b_ik_times_t_ik.iter())
-                .map(|(a, b)| a + b)
-                .collect();
-        }
-    }
-
-    // Second summation: ∑ C_ijk * g_ij^(k)
-    // Calculate u1 using the pre-generated c_matrix
-    for i in 0..size_r.value() {
-        for j in i..size_r.value() {
-            for k in 0..t2.value() {
-                let c_i_j_k = &c_matrix[i][j][k];
-                let g_i_j = &g_matrix_aggregated[i][j];
-                let c_i_j_k_times_g_i_j = c_i_j_k.values
-                    .iter()
-                    .map(|row| {
-                        row.iter()
-                            .zip(g_i_j.iter())
-                            .map(|(c, g)| c * g)
-                            .fold(
-                                PolynomialRing {
-                                    coefficients: vec![Zq::from(0); size_n.value()],
-                                },
-                                |acc, val| acc + val,
-                            )
-                    })
-                    .collect::<Vec<PolynomialRing>>();
-                u1 = u1
-                    .iter()
-                    .zip(c_i_j_k_times_g_i_j.iter())
-                    .map(|(a, b)| a + b)
-                    .collect();
-            }
-        }
-    }
-
 
     // ================================================
 
